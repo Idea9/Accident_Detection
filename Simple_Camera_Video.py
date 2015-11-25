8f1d4ea1c75c54b31b870e8653b88ee499f74246
Idea9 <puzikfx@gmail.com>
2015-11-25 23:02:36 +0100

    working capture video from stream

--- Simple_Camera_Video.py
+++ Simple_Camera_Video.py
@@ -1,17 +1,23 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("http://149.156.199.210:8080/mjpg/video.mjpg")
-
+cap = cv2.VideoCapture(0)
 # easy get video to "cap" variable from 0 -> means local camera
-while cap.isOpened():
+while(True):
     ret, frame = cap.read()
     # read() zwraca 2 wartosci, jedna to true dla poprawnie odebranej ramki, druga to sama ramka w postaci macierzy
+
     print cap.get(4)
     # get wyswietla parametry na temat plik, jest ich 18(w dok/tut), 4 to dla przykladu "width" klatki
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('window', gray)
+    cap.set(3, 480)
+    cap.set(4, 360)
+    # set moze ustawiac te parametry ktore sa w get(), zmiana kazdej klatki na inna to jednak nie najlepszy pomysl toche muli dziada
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # po prostu bierzesz kazdy frame, przekladasz mu kolory i zapisujesz do gray
+
+    cv2.imshow('frame',gray)
     #downie pierwszy czlon to po prosu nazwa okna, drugi to frame do wyswietlenia
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
