12fb2f9b1051913741d56a125543bebff8090412
Idea9 <puzikfx@gmail.com>
2015-11-25 15:58:43 +0100

    get and show video from camera

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread("img/jennifer.jpg")
-b,g,r = cv2.split(img)
-img2 = cv2.merge([r,g,b])
-# img2 = np.array(img)
-# img2 = img2[:,:,::-1]
-print b
-plt.imshow(img2, interpolation = 'bicubic')
-plt.xticks([])
-plt.yticks([])
-plt.show()
+cap = cv2.VideoCapture(0)
+# easy get video to "cap" variable from 0 -> means local camera
+while(True):
+    ret, frame = cap.read()
+    # read() zwraca 2 wartosci, jedna to true dla poprawnie odebranej ramki, druga to sama ramka w postaci macierzy
+
+    print cap.get(4)
+    # get wyswietla parametry na temat plik, jest ich 18(w dok/tut), 4 to dla przykladu "width" klatki
+
+    cap.set(3, 480)
+    cap.set(4, 360)
+    # set moze ustawiac te parametry ktore sa w get(), zmiana kazdej klatki na inna to jednak nie najlepszy pomysl toche muli dziada
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # po prostu bierzesz kazdy frame, przekladasz mu kolory i zapisujesz do gray
+
+    cv2.imshow('frame',gray)
+    #downie pierwszy czlon to po prosu nazwa okna, drugi to frame do wyswietlenia
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
