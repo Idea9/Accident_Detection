92e02ba67d2008935e0af2d4e2bc42177368a60f
Idea9 <puzikfx@gmail.com>
2015-11-26 14:39:56 +0100

    Get_stream and save it

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import cv2
 
 cap = cv2.VideoCapture("http://149.156.199.210:8080/mjpg/video.mjpg")
 
-# easy get video to "cap" variable from 0 -> means local camera
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (int(cap.get(3)),int(cap.get(4))))
+
+# easy get video to "cap" varqiable from 0 -> means local camera
 while cap.isOpened():
     ret, frame = cap.read()
-    # read() zwraca 2 wartosci, jedna to true dla poprawnie odebranej ramki, druga to sama ramka w postaci macierzy
-    print cap.get(4)
-    # get wyswietla parametry na temat plik, jest ich 18(w dok/tut), 4 to dla przykladu "width" klatki
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('window', gray)
-    #downie pierwszy czlon to po prosu nazwa okna, drugi to frame do wyswietlenia
+    if ret:
+        # read() zwraca 2 wartosci, jedna to true dla poprawnie odebranej ramki, druga to sama ramka w postaci macierzy
+        print cap.get(4)
+        # get wyswietla parametry na temat plik, jest ich 18(w dok/tut), 4 to dla przykladu "width" klatki
+        out.write(frame)
+        cv2.imshow('window', frame)
+        #downie pierwszy czlon to po prosu nazwa okna, drugi to frame do wyswietlenia
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-
 # When everything done, release the capture
+out.release()
 cap.release()
 cv2.destroyAllWindows()
