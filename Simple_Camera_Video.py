62d60ddf1efeb87727b6e1813a84769b931aa549
Idea9 <puzikfx@gmail.com>
2015-12-04 11:58:23 +0100

    Good Video Sample and drawing tracks of going cars

--- Simple_Camera_Video.py
+++ Simple_Camera_Video.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("video/ScreenCaptureProject.mp4")
 # easy get video to "cap" variable from 0 -> means local camera
 while(True):
     ret, frame = cap.read()
