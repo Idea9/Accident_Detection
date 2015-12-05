b3462bf185e0d30128bfe7bf1cde17f951a09b08
Idea9 <puzikfx@gmail.com>
2015-12-05 14:15:16 +0100

    separate file into frames

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import numpy as np
-import cv2
+import re
+import array
+vectorFile = open("vectors.txt")
+frameStartIndexes = []
+frames = []
+last = 0
+def remove_values_from_list(the_list, val):
+   return [value for value in the_list if value != val]
 
+def seperate_list(list, newFramePoint):
+    return 0;
+
+pureVectorFile = remove_values_from_list(vectorFile, "\n")
+for line in pureVectorFile:
+    if re.search("P ", line):
+        frameStartIndexes.append(pureVectorFile.index(line))
+
+for new in frameStartIndexes:
+    if last:
+        frames.append(pureVectorFile[last:new])
+    last = new
