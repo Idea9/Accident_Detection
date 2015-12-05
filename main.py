9b71b50c86afbf83f80f678a441022dd1faf2a2a
Idea9 <puzikfx@gmail.com>
2015-12-05 15:24:59 +0100

    bug detected

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def seperate_list(frame, file, newFramePoint):
     last = 0
     for new in newFramePoint:
         if last:
-            frames.append(file[last:new])
+            frames.append(file[(last+1):new])
         last = new
 
 pureVectorFile = remove_values_from_list(vectorFile, "\n")
@@ -21,5 +21,10 @@ for line in pureVectorFile:
         frameStartIndexes.append(pureVectorFile.index(line))
 
 seperate_list(frames, pureVectorFile, frameStartIndexes)
-
-print frames[0]
+i = 0
+for frame in frames:
+    i += 1
+    print "nowy kurwa frame", i
+    for macroblock in frame:
+        macroblock = macroblock.split()
+        print ((float)(macroblock[2]) * (float)(macroblock[2])) + ((float)(macroblock[3]) + (float)(macroblock[3]))
