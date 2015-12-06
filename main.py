796556b52606f06f1faf9216d67eb661069b8ac9
Idea9 <puzikfx@gmail.com>
2015-12-06 11:25:56 +0100

    Wektory ruchu w obszarach

--- main.py
+++ main.py
@@ -33,15 +33,44 @@ for line in pureVectorFile:
 # Dzielimy otworzony plik na ramki
 seperate_list(frames, pureVectorFile, frameStartIndexes)
 
-i = 0
-totalMotion = []
-vectorLength = []
+
 for frame in frames:
-    vLength = []
+    area0, area1, area2, area3, area4, area5, area6, area7, area8 = ([] for i in range(9))
     for macroblock in frame:
         macroblock = macroblock.split()
-        vLength.append(vector_length(macroblock[2], macroblock[3]))
-    vectorLength.append(vLength)
-    totalMotion.append(sum(vLength))
-
-print totalMotion
+        x = int(macroblock[0])
+        y = int(macroblock[1])
+        currentVector = vector_length(macroblock[2], macroblock[3])
+        if x<107 and y<66:
+            area0.append(currentVector)
+        elif x<210 and x>=107 and y<66:
+            area1.append(currentVector)
+        elif x>210 and y<66:
+            area2.append(currentVector)
+        elif x<107 and y<126 and y>=67:
+            area3.append(currentVector)
+        elif x<210 and x>=107 and y<126 and y>=67:
+            area4.append(currentVector)
+        elif x>210 and y<126 and y>=67:
+            area5.append(currentVector)
+        elif x<107 and y>=126:
+            area6.append(currentVector)
+        elif x<210 and x>=107 and y>=126:
+            area7.append(currentVector)
+        elif x>210 and y>=126:
+            area8.append(currentVector)
+        else:
+            print "no kurwa sprobuj"
+    print len(area0), len(area1), len(area2), len(area3), len(area4), len(area5), len(area6), len(area7), len(area8)
+# i = 0
+# totalMotion = []
+# vectorLength = []
+# for frame in frames:
+#     vLength = []
+#     for macroblock in frame:
+#         macroblock = macroblock.split()
+#         vLength.append(vector_length(macroblock[2], macroblock[3]))
+#     vectorLength.append(vLength)
+#     totalMotion.append(sum(vLength))
+#
+# print totalMotion
