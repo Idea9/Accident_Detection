e94d2f0417b64a37afa35d9baf5e788183ae0356
Idea9 <puzikfx@gmail.com>
2015-12-06 09:41:33 +0100

    Sprzatanie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-import array
+import math
+
 vectorFile = open("vectors.txt")
 test = open("text.txt", "r+")
 frameStartIndexes = []
@@ -9,6 +10,7 @@ frames = []
 def remove_values_from_list(the_list, val):
    return [value for value in the_list if value != val]
 
+# Funkcja rozdzielajaca liste na mniejsze porcje wg index'u wskazanych punktow
 def seperate_list(frame, file, newFramePoint):
     last = 0
     for new in newFramePoint:
@@ -16,17 +18,21 @@ def seperate_list(frame, file, newFramePoint):
             frames.append(file[(last+1):new])
         last = new
 
+# Usuwamy z otworzonego pliku puste linie
 pureVectorFile = remove_values_from_list(vectorFile, "\n")
 
+# Szukamy poczatku ramek P, niezbedne do podzielenia pliku
 for line in pureVectorFile:
     if re.search("P ", line):
         frameStartIndexes.append(pureVectorFile.index(line))
 
+# Dzielimy otworzony plik na ramki
 seperate_list(frames, pureVectorFile, frameStartIndexes)
+
 i = 0
 for frame in frames:
     i += 1
     print "nowy kurwa frame", i
     for macroblock in frame:
         macroblock = macroblock.split()
-        print ((float)(macroblock[2]) * (float)(macroblock[2])) + ((float)(macroblock[3]) + (float)(macroblock[3]))
+        print math.sqrt(((float)(macroblock[2]) * (float)(macroblock[2])) + ((float)(macroblock[3]) * (float)(macroblock[3])))
