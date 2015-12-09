0a729eafefbb9954c87330aba8091461e10ff022
Idea9 <puzikfx@gmail.com>
2015-12-09 10:34:17 +0100

    Core

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import numpy as np
 import re
 import math
 
-vectorFile = open("vectors.txt")
-test = open("text.txt", "r+")
+vectorFile = open("Vectors/Man_in_background.txt")
 frameStartIndexes = []
 frames = []
 
@@ -42,46 +42,46 @@ for frame in frames:
         macroblock = macroblock.split()
         vLength.append(vector_length(macroblock[2], macroblock[3]))
     vectorLength.append(vLength)
-    totalMotion.append(sum(vLength))
+    totalMotion.append(sum(vLength) + 0.000001)
 
 # Kazda ramka dzielona jest na 9 blokow o rownych rozmiarach
 # Do kazdego z bloku wpisywana jest suma dlugosci wekktorow ruchu w tym bloku
 # Wszystkie Bloki z ramki zapisujemy do jednej listy
-# areaMotion = []
-# i = 0
-# for frame in frames:
-#     area0, area1, area2, area3, area4, area5, area6, area7, area8 = ([] for i in range(9))
-#     FrameByArea = []
-#     for macroblock in frame:
-#         macroblock = macroblock.split()
-#         x = int(macroblock[0])
-#         y = int(macroblock[1])
-#         currentVector = vector_length(macroblock[2], macroblock[3])
-#         if x<107 and y<66:
-#             area0.append(currentVector)
-#         elif x<210 and x>=107 and y<66:
-#             area1.append(currentVector)
-#         elif x>210 and y<66:
-#             area2.append(currentVector)
-#         elif x<107 and y<126 and y>=67:
-#             area3.append(currentVector)
-#         elif x<210 and x>=107 and y<126 and y>=67:
-#             area4.append(currentVector)
-#         elif x>210 and y<126 and y>=67:
-#             area5.append(currentVector)
-#         elif x<107 and y>=126:
-#             area6.append(currentVector)
-#         elif x<210 and x>=107 and y>=126:
-#             area7.append(currentVector)
-#         elif x>210 and y>=126:
-#             area8.append(currentVector)
-#         else:
-#             print "makroblok nie pasuje do zadnego obszaru"
-#     FrameByArea.extend((sum(area0)/totalMotion[i], sum(area1)/totalMotion[i], sum(area2)/totalMotion[i], sum(area3)/totalMotion[i], sum(area4)/totalMotion[i], sum(area5)/totalMotion[i], sum(area6)/totalMotion[i], sum(area7)/totalMotion[i], sum(area8)/totalMotion[i]))
-#     areaMotion.append(FrameByArea)
-#     i += 1
-#
-# print areaMotion
+areaMotion = []
+i = 0
+for frame in frames:
+    area0, area1, area2, area3, area4, area5, area6, area7, area8 = ([] for i in range(9))
+    FrameByArea = []
+    for macroblock in frame:
+        macroblock = macroblock.split()
+        x = int(macroblock[0])
+        y = int(macroblock[1])
+        currentVector = vector_length(macroblock[2], macroblock[3])
+        if x < 107 and y <= 66:
+            area0.append(currentVector)
+        elif 210 > x >= 107 and y <= 66:
+            area1.append(currentVector)
+        elif x >= 210 and y <= 66:
+            area2.append(currentVector)
+        elif x < 107 and 126 > y >= 67:
+            area3.append(currentVector)
+        elif 210 > x >= 107 and 126 > y >= 67:
+            area4.append(currentVector)
+        elif x >= 210 and 126 > y >= 67:
+            area5.append(currentVector)
+        elif x < 107 and y >= 126:
+            area6.append(currentVector)
+        elif 210 > x >= 107 and y >= 126:
+            area7.append(currentVector)
+        elif x >= 210 and y >= 126:
+            area8.append(currentVector)
+        else:
+            print x, y
+    FrameByArea.extend((sum(area0)/totalMotion[i], sum(area1)/totalMotion[i], sum(area2)/totalMotion[i], sum(area3)/totalMotion[i], sum(area4)/totalMotion[i], sum(area5)/totalMotion[i], sum(area6)/totalMotion[i], sum(area7)/totalMotion[i], sum(area8)/totalMotion[i]))
+    areaMotion.append(FrameByArea)
+    i += 1
+
+print areaMotion
 
 i = 0
 angleMotion = []
@@ -115,4 +115,6 @@ for frame in frames:
     angleMotion.append(FrameByAngle)
     i += 1
 
-print angleMotion
+print max(totalMotion)
+print min(totalMotion)
+print sum(totalMotion)/len(totalMotion)
