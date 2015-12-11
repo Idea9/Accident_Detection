79b3169ab192cb7f5e587f49be81dfd3c1296c1b
Idea9 <puzikfx@gmail.com>
2015-12-11 17:42:34 +0100

    Pokazywanie wycinku pliku, porzadkowanie do funkcji

---
+++ Opencv.py
@@ -0,0 +1,23 @@
+import cv2
+
+def showVideoPart(fileName, startFrame, endFrame):
+
+    cap = cv2.VideoCapture("video/" + fileName)
+
+    if endFrame <= startFrame:
+        print "bledna liczba ramek"
+    if endFrame > cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+        print "Na pewno to Video, nie ma tylu ramek"
+
+    frameNumber = 0
+    while cap.isOpened():
+        frameNumber += 1
+        ret, frame = cap.read()
+
+        if startFrame < frameNumber < endFrame:
+            cv2.imshow('window', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import cv2
+from Opencv import showVideoPart
 
-
-def showVideoPart(filename, startFrame, endFrame):
-
-    cap = cv2.VideoCapture("video/Man_in_background.avi")
-
-    if endFrame <= startFrame:
-        print "bledna liczba ramek"
-    if endFrame > cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
-        print "Na pewno to Video, nie ma tylu ramek"
-
-    startFrame = 100
-    endFrame = 200
-    frameNumber = 0
-    while cap.isOpened():
-        frameNumber += 1
-        ret, frame = cap.read()
-
-        if startFrame < frameNumber < endFrame:
-            cv2.imshow('window', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    print "naliczylo ramek: ", frameNumber
-    cap.release()
-    cv2.destroyAllWindows()
+showVideoPart("Pure_Traffic.avi", 200, 500)
