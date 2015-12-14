a9d75712d1c46722d5c8ba23eeb8a71f03c55643
Idea9 <puzikfx@gmail.com>
2015-12-14 18:29:52 +0100

    SVM_Tests

---
+++ Cechy_ruchu_no_zero.py
@@ -0,0 +1,139 @@
+import numpy as np
+import re
+import math
+
+
+def get_motion_feature(file, startFrame, endFrame, returnOption):
+    vectorFile = open("Vectors/" + file + ".txt")
+    frameStartIndexes = []
+    frames = []
+
+    # Funckja do dowolnego lini o danym stringu
+    def remove_values_from_list(the_list, val):
+        return [value for value in the_list if value != val]
+
+    # Funkcja rozdzielajaca liste na mniejsze porcje wg index'u wskazanych punktow
+    def seperate_list(frame, file, newFramePoint):
+        last = 0
+        for new in newFramePoint:
+            if last:
+                frames.append(file[(last + 1):new])
+            last = new
+
+    # Funkcja liczaca dlugosc wektora
+    def vector_length(x, y):
+        return math.sqrt(float(x) * float(x) + float(y) * float(y))
+
+    # Usuwamy z otworzonego pliku puste linie
+    pureVectorFile = remove_values_from_list(vectorFile, "\n")
+
+    # Szukamy poczatku ramek P, niezbedne do podzielenia pliku
+    frameNumber = 0
+    for line in pureVectorFile:
+        if re.search("P ", line):
+            frameNumber += 1
+            if startFrame < frameNumber < endFrame:
+                frameStartIndexes.append(pureVectorFile.index(line))
+
+    # Dzielimy otworzony plik na ramki
+    seperate_list(frames, pureVectorFile, frameStartIndexes)
+
+    # Obliczamy tottalny ruch w calej ramce
+    totalMotion = np.array([])
+    vectorLength = []
+    badFrames = []
+    for frameId, frame in enumerate(frames):
+        vLength = []
+        for macroblock in frame:
+            macroblock = macroblock.split()
+            vLength.append(vector_length(macroblock[2], macroblock[3]))
+        vectorLength.append(vLength)
+        summary = sum(vLength) + 0.000001
+        if summary > 10:
+            totalMotion = np.append(totalMotion, summary)
+        else:
+            badFrames.append(frameId)
+
+    print "ilosc zlych ramek", len(badFrames)
+    i = 0
+    angleMotion = []
+    sampleAngle = np.array([])
+    for frameId, frame in enumerate(frames):
+        if not badFrames:
+            angle0, angle1, angle2, angle3, angle4, angle5, angle6, angle7 = ([] for i in range(8))
+            FrameByAngle = []
+            for macroblock in frame:
+                macroblock = macroblock.split()
+                currentVector = vector_length(macroblock[2], macroblock[3])
+                if currentVector != 0:
+                    currentAngle = math.degrees(math.atan2(float(macroblock[3]), float(macroblock[2])))
+                    if -22.5 <= currentAngle < 22.5:
+                        angle0.append(currentVector)
+                    elif 22.5 <= currentAngle < 67.5:
+                        angle1.append(currentVector)
+                    elif 67.5 <= currentAngle < 112.5:
+                        angle2.append(currentVector)
+                    elif 112.5 <= currentAngle < 157.5:
+                        angle3.append(currentVector)
+                    elif 157.5 <= currentAngle <= 180.0 or -180.0 <= currentAngle < -157.5:
+                        angle4.append(currentVector)
+                    elif -157.5 <= currentAngle < -112.5:
+                        angle5.append(currentVector)
+                    elif -112.5 <= currentAngle < -67.5:
+                        angle6.append(currentVector)
+                    elif -67.5 <= currentAngle < -22.5:
+                        angle7.append(currentVector)
+                    else:
+                        print "makroblok nie pasuje do zadnego obszaru"
+            FrameByAngle.extend((sum(angle0) / totalMotion[i], sum(angle1) / totalMotion[i], sum(angle2) / totalMotion[i],
+                                 sum(angle3) / totalMotion[i], sum(angle4) / totalMotion[i], sum(angle5) / totalMotion[i],
+                                 sum(angle6) / totalMotion[i], sum(angle7) / totalMotion[i]))
+            angleMotion.append(FrameByAngle)
+            sampleAngle = np.append(sampleAngle, sum(angle2))
+            i += 1
+        elif frameId != badFrames[0]:
+            angle0, angle1, angle2, angle3, angle4, angle5, angle6, angle7 = ([] for i in range(8))
+            FrameByAngle = []
+            for macroblock in frame:
+                macroblock = macroblock.split()
+                currentVector = vector_length(macroblock[2], macroblock[3])
+                if currentVector != 0:
+                    currentAngle = math.degrees(math.atan2(float(macroblock[3]), float(macroblock[2])))
+                    if -22.5 <= currentAngle < 22.5:
+                        angle0.append(currentVector)
+                    elif 22.5 <= currentAngle < 67.5:
+                        angle1.append(currentVector)
+                    elif 67.5 <= currentAngle < 112.5:
+                        angle2.append(currentVector)
+                    elif 112.5 <= currentAngle < 157.5:
+                        angle3.append(currentVector)
+                    elif 157.5 <= currentAngle <= 180.0 or -180.0 <= currentAngle < -157.5:
+                        angle4.append(currentVector)
+                    elif -157.5 <= currentAngle < -112.5:
+                        angle5.append(currentVector)
+                    elif -112.5 <= currentAngle < -67.5:
+                        angle6.append(currentVector)
+                    elif -67.5 <= currentAngle < -22.5:
+                        angle7.append(currentVector)
+                    else:
+                        print "makroblok nie pasuje do zadnego obszaru"
+            FrameByAngle.extend((sum(angle0) / totalMotion[i], sum(angle1) / totalMotion[i], sum(angle2) / totalMotion[i],
+                                 sum(angle3) / totalMotion[i], sum(angle4) / totalMotion[i], sum(angle5) / totalMotion[i],
+                                 sum(angle6) / totalMotion[i], sum(angle7) / totalMotion[i]))
+            angleMotion.append(FrameByAngle)
+            sampleAngle = np.append(sampleAngle, sum(angle2))
+            i += 1
+        else:
+            badFrames.pop(0)
+
+    if returnOption == 0:
+        print len(totalMotion), " probek ruchu calkowitego"
+        return totalMotion
+    elif returnOption == 1:
+        print len(totalMotion), " probek ruchu katowego"
+        return sampleAngle
+    else:
+        return -1
+        print "nie ma takiej opcji"
+
+
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import numpy as np
 from sklearn import svm
 
-from Cechy_ruchu import get_motion_feature
+from Cechy_ruchu_no_zero import get_motion_feature
 
-normalTotalMotion = get_motion_feature("Pure_Traffic1", 50, 350, 0)
+
+def check_result(condition, results):
+    ok = 0
+    for result in results:
+        if result == condition:
+            ok += 1
+    return float(ok)/len(results)*100, "%"
+
+
+normalTotalMotion = get_motion_feature("Pure_Traffic1", 250, 1550, 0)
 abnormalTotalMotion = get_motion_feature("Man_through_road4", 70, 160, 0)
 
-normalAngleMotion = get_motion_feature("Pure_Traffic1", 50, 350, 1)
+normalAngleMotion = get_motion_feature("Pure_Traffic1", 250, 1550, 1)
 abnormalAngleMotion = get_motion_feature("Man_through_road4", 70, 160, 1)
 
 normal = np.zeros(len(normalTotalMotion))
@@ -15,12 +24,13 @@ abnormal = np.ones(len(abnormalTotalMotion))
 X = np.column_stack((np.append(normalTotalMotion, abnormalTotalMotion), np.append(normalAngleMotion, abnormalAngleMotion)))
 y = np.append(normal, abnormal)
 
-clf = svm.SVC()
+clf = svm.SVC(class_weight={1: 50})
 print clf.fit(X, y)
 
-testTotalMotion = get_motion_feature("Pure_Traffic3", 1350, 1550, 0)
-testAngleMotion = get_motion_feature("Pure_Traffic3", 1350, 1550, 1)
+testTotalMotion = get_motion_feature("Man_through_road2", 1350, 1550, 0)
+testAngleMotion = get_motion_feature("Man_through_road2", 1350, 1550, 1)
 
 test = np.column_stack((testTotalMotion, testAngleMotion))
 
 print clf.predict(test)
+print check_result(1, clf.predict(test))
